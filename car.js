7092b68ec37ff039675a1c44fbb595775cf9805f
Julian Chung <juliancwt77@gmail.com>
2016-10-10 14:36:07 +0800

    add accelerate and color

--- car.js
+++ car.js
@@ -3,7 +3,14 @@ var Car = function (color, convertible) {
   this.color = color
   this.speed = 0
   this.convertible = convertible
+  this.accelerate = function(addspeed) {
 
-}
+      this.speed += addspeed
 
+    }
+   this.setColor = function(addColor) {
+
+      this.color = addColor
+   }
+ }
 module.exports = Car
