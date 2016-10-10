73fb5da3e69252864c85794f983d1f9776e252cf
terence lim <terencelim92@gmail.com>
2016-10-10 14:36:42 +0800

    added decelerate and stop functions

--- car.js
+++ car.js
@@ -2,6 +2,12 @@ var Car = function (color, convertible) {
   this.color = color
   this.speed = 0
   this.convertible = convertible
+  this.decelerate = function (speedDown) {
+    this.speed -= speedDown
+  }
+  this.stop = function () {
+    this.speed = 0
+  }
 }
 
 module.exports = Car
